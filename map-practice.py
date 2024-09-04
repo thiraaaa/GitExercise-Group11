197c704789678127b85bd22737c8f99e485497b3
Nur A'liah Athirah <n.aliah.amn@gmail.com>
2024-09-05 01:33:08 +0800

    Install pandas

--- map-practice.py
+++ map-practice.py
@@ -1,3 +1,4 @@
+from email.policy import strict
 from flask import Flask
 import folium
 
@@ -8,12 +9,12 @@ def base():
     map = folium.Map(location=[2.9279623186421695, 101.64200330740373])
     return map._repr_html_()
 
-@app.route("/map.marker")
+
+@app.route("/map-marker")
 def map_marker():
-     map = folium.Map(location=[2.9279623186421695, 101.64200330740373], tiles='Stamen Terrain', zoom_start=12)
-    
+    map = folium.Map(location=[2.9279623186421695, 101.64200330740373], tiles='Stamen Terrain', zoom_start=12)
     folium.Marker(location=[2.9279623186421695, 101.64200330740373], popup="<i>MMU Cyberjaya</i>", tooltip="Click Here").add_to(map)
-    
+
     return map._repr_html_()
 
 if __name__ == "__main__":
