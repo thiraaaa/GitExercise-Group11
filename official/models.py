b7b95f24cafd50bbd767e642653d4d0ad4a5278f
Nur A'liah Athirah <n.aliah.amn@gmail.com>
2024-09-28 22:33:55 +0800

    Added official to main branch

---
+++ official/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Place(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    photos = db.relationship('Photo', backref='place', lazy=True)
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200), nullable=True)  
+    review = db.Column(db.Text, nullable=True)
+    likes = db.Column(db.Integer, default=0)
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
